e2d732796a4a72228296d6aa78b959a39c0628f4
ljm <260960918@qq.com>
2019-06-30 19:05:39 +0800

    add mathprocess

---
+++ modules/process/include/process/mathprocess.h
@@ -0,0 +1,59 @@
+/*************************************************************************
+	> File Name: mathprocess.h
+	> Author: ljm
+	> Mail: jimin@iscas.ac.cn
+	> Created Time: 2019年06月30日 星期日 16时20分49秒
+ ************************************************************************/
+//
+/**
+* @brief      calculate the mean value of vector x. 
+* @param[in]  type        template data type.
+* @param[in]  x           vector x.
+* @param[in]  len         length of vector x.
+**/
+template <class type>
+float mean(type* x, int len)
+{
+	float res = 0;
+	for(int i=0; i<len; i++)
+		res = res +(float)x[i];
+	res = res/len;
+	return res;
+}
+
+/**
+* @brief      calculate the variance value of vector x. 
+* @param[in]  type        template data type.
+* @param[in]  x           vector x.
+* @param[in]  len         length of vector x.
+**/
+template <class type>
+float variance(type* x, int len)
+{
+	float res = 0;
+	float m = mean<type>(x,len);
+	for(int i=0; i<len; i++)
+		res = res +(float)((x[i]-m)*(x[i]-m));
+	res = res/len;
+	return res;
+}
+
+/**
+* @brief      calculate the covariance value of vector x. 
+* @param[in]  type        template data type.
+* @param[in]  x           vector x.
+* @param[in]  y           vector y.
+* @param[in]  len         length of vector x.
+**/
+template <class type1,class type2>
+float covariance(type1* x, type2* y, int len)
+{
+	float res = 0;
+	float mx = mean<type1>(x,len);
+	float my = mean<type2>(y,len);
+	for(int i=0; i<len; i++)
+		res += (x[i]-mx)(y[i]-my);
+	return res/len;
+}
+
+
--- modules/process/include/process/process.h
+++ modules/process/include/process/process.h
@@ -1,3 +1,4 @@
+#include "mathprocess.h"
 #include "imageprocess.h"
 #include "matrixprocess.h"
 #include "spectralprocess.h"
