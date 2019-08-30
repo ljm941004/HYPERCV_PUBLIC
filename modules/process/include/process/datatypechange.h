a3dd95a8673fad734816784ef711f42a45aef736
ljm <260960918@qq.com>
2019-08-30 09:50:57 +0800

    add change data type

--- modules/process/include/process/datatypechange.h
+++ modules/process/include/process/datatypechange.h
@@ -5,4 +5,13 @@
  ************************************************************************/
 
 
-hyper_mat hyper_mat_uchar2float(hyper_mat f_mat);
+hyper_mat hyper_mat_float2char(hyper_mat f_mat);
+
+hyper_mat hyper_mat_float2int(hyper_mat f_mat);
+
+
+hyper_mat hyper_mat_float2short(hyper_mat f_mat);
+
+
+hyper_mat hyper_mat_float2ushort(hyper_mat f_mat);
+
--- modules/process/src/datatypechange.c
+++ modules/process/src/datatypechange.c
@@ -8,16 +8,16 @@
 
 hyper_mat hyper_mat_float2int(hyper_mat f_mat)
 {
-	_assert(f_mat->data_type == 4,"intput mat datatype == float")
+	_assert(f_mat->data_type == 4,"intput mat datatype == float");
 	int samples = f_mat->samples;
 	int lines = f_mat->lines;
 	int bands = f_mat->bands;
 
 	hyper_mat dst = create_hyper_mat(samples,lines,bands,3,f_mat->interleave);
-	
+
 	int * data = dst->data;
 	float* fdata = f_mat->data;
-	
+
 	int datasize = samples*lines*bands;
 
 	for(int i=0;i<datasize;i++)
@@ -26,27 +26,27 @@ hyper_mat hyper_mat_float2int(hyper_mat f_mat)
 	return dst;
 }
 
-hyper_mat hyper_mat_float2uchar(hyper_mat f_mat)
+hyper_mat hyper_mat_float2char(hyper_mat f_mat)
 {
 	_assert(f_mat->data_type == 4,"intput mat datatype == float")
-	int samples = f_mat->samples;
+		int samples = f_mat->samples;
 	int lines = f_mat->lines;
 	int bands = f_mat->bands;
 
 	hyper_mat dst = create_hyper_mat(samples,lines,bands,1,f_mat->interleave);
-	
-	unsigned char* data = dst->data;
+
+	char* data = dst->data;
 	float* fdata = f_mat->data;
 	int tmp = 0;
 	int datasize = samples*lines*bands;
 	for(int i=0;i<datasize;i++)
 	{
 		tmp = HYPERCV_ROUND(fdata[i]);
-	
-		if(tmp>255)
-			data[i] = 255;
-		else if(tmp<0)
-			data[i] = 0;
+
+		if(tmp>127)
+			data[i] = 128;
+		else if(tmp<-127)
+			data[i] = -127;
 		else
 			data[i] = tmp;
 
@@ -54,7 +54,45 @@ hyper_mat hyper_mat_float2uchar(hyper_mat f_mat)
 	return dst;
 }
 
-
-hyper_mat hyper_mat_short2float(hyper_mat f_mat)
+hyper_mat hyper_mat_float2short(hyper_mat f_mat)
 {
+	_assert(f_mat->data_type == 4,"intput mat datatype == float");
+    int samples = f_mat->samples;
+	int lines = f_mat->lines;
+	int bands = f_mat->bands;
+
+	hyper_mat dst = create_hyper_mat(samples,lines,bands,2,f_mat->interleave);
+
+	short* data = dst->data;
+	float* fdata = f_mat->data;
+	int tmp = 0;
+	int datasize = samples*lines*bands;
+	for(int i=0;i<datasize;i++)
+		data[i] =(short)HYPERCV_ROUND(fdata[i]);
+	return dst;
 }
+
+hyper_mat hyper_mat_float2ushort(hyper_mat f_mat)
+{
+	_assert(f_mat->data_type == 4,"intput mat datatype == float");
+	int samples = f_mat->samples;
+	int lines = f_mat->lines;
+	int bands = f_mat->bands;
+
+	hyper_mat dst = create_hyper_mat(samples,lines,bands,12,f_mat->interleave);
+
+	unsigned short* data = dst->data;
+	float* fdata = f_mat->data;
+	int tmp = 0;
+	int datasize = samples*lines*bands;
+	for(int i=0;i<datasize;i++)
+	{
+		tmp = HYPERCV_ROUND(fdata[i]);
+		if(tmp<0)
+			data[i] =0;
+		else
+			data[i] = tmp;
+	}
+	return dst;
+}
+
