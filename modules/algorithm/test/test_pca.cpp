11ffe7fe69546258cb1c80f2259d971c52a35433
ljm <260960918@qq.com>
2019-11-07 17:30:36 +0800

    add todo

--- modules/algorithm/test/test_pca.cpp
+++ modules/algorithm/test/test_pca.cpp
@@ -6,58 +6,22 @@
 #include "precomp.h"
 
 using namespace std;
-#ifndef TEST_HYPERCV_PERFORMANCE
-#define ITERS 1
-#else
-#define ITES 20
-#endif
-
-#ifndef TEST_HYPERCV_PERFORMANCE
-#define SAMPLES_START 10
-#define SAMPLES_END   11
-#define LINES_START  10
-#define LINES_END    11
-#define BANDS_START  10
-#define BANDS_END    11
-
-#else
-#define SAMPLES_START 10
-#define SAMPLES_END   11
-#define LINES_START  10
-#define LINES_END    11
-#define BANDS_START  10
-#define BANDS_END    11
-#endif
-
-static hyper_mat src_mat = NULL;
-static hyper_mat dst_mat = NULL;
-
-
-static void hypercv_test_setup(int samples, int lines, int bands, int data_type, char interleave[])
-{
-  	src_mat = create_hyper_mat(samples, lines, bands, data_type, interleave);
-	hypercv_dataInit<unsigned char>((unsigned char*)src_mat->data,samples,lines,bands);
-}
-
-static void test_delete_hyper_mat()
-{
-	delete_hyper_mat(src_mat);
-	delete_hyper_mat(dst_mat);
-}
-
 
 void test_pca()
 {
-	
-	hypercv_test_setup(3,3,3,4,"bip");
-	dst_mat = hyper_mat_copy(src_mat);
-	hyper_mat_pca(src_mat,dst_mat,20);
+    hyper_mat t = hmread_with_hdr("/home/ljm/data/dataz/bd0z","/home/ljm/data/dataz/bd0z.hdr");
+
+	hyper_mat float_mat =hyper_mat_ushort2float(t);
+
+	delete_hyper_mat(t);
+	hyper_mat dst_mat = hyper_mat_copy(float_mat);
+	hyper_mat_pca(float_mat,dst_mat,1);
 	hmwrite("pca",dst_mat);
-	test_delete_hyper_mat();
+	delete_hyper_mat(float_mat);
+	delete_hyper_mat(dst_mat);
 }
 
 TEST(ALGORITHM,PCA)
 {
 	test_pca();
-
 }
