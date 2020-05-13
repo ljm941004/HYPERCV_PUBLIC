56834e73b6672de7e82385e95e76a07073f086d9
ljm <260960918@qq.com>
2020-05-13 23:35:20 +0800

    add attitude

--- modules/satellite/include/satellite/satellite_attitude.h
+++ modules/satellite/include/satellite/satellite_attitude.h
@@ -4,4 +4,18 @@
 	> Mail: jimin@iscas.ac.cn 
 	> Created Time: 2020年05月11日 星期一 20时29分06秒
  ************************************************************************/
+struct quaternion
+{
+	double q1;
+	double q2;
+	double q3;
+	double q4;
+};
+ 
+struct Euler_angle
+{
+	double x;  // 滚动角x，
+	double y;  // 俯仰角y
+	double z;  // 偏航角z
+};
 
